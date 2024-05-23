af8ad1b59a5316dd4e12103d529bb17f953a0109
S-Borozna <ya.svetlana17@gmail.com>
2024-05-23 19:50:00 +0300

    1

--- locators/locators_main_page.py
+++ locators/locators_main_page.py
@@ -8,7 +8,7 @@ class MainPage(WebPage):
 
     def __init__(self, web_driver, url=''):
         if not url:
-            url = os.getenv("MAIN_URL") or 'https://lakikraski.by/'
+            url = os.getenv("MAIN_URL") or 'https://hoster.by/'
 
         super().__init__(web_driver, url)
 
@@ -21,4 +21,16 @@ class MainPage(WebPage):
     btn_headers_about_company = WebElement (xpath = '((//*[@data-widget_type="nav-menu.default"])[1]//'
                                                     '*[@aria-expanded="false"])[7]')
     btn_headers_contact = WebElement (xpath = '((//*[@data-widget_type="nav-menu.default"])[1]//a)[23]')
+    btn_headers_basket = WebElement (xpath = '(//*[@data-widget_type="woocommerce-menu-cart.default"])[1]//i')
+    btn_headers_profile = WebElement (xpath = '(//*[@data-widget_type="icon.default"])[1]//*[@aria-hidden="true"]')
+    btn_footer_arch_paints = WebElement (xpath = '(//*[@data-elementor-type="footer"]/*[2]//span)[8]')
+    btn_footer_ind_coatings = WebElement (xpath = '(//*[@data-elementor-type="footer"]/*[2]//span)[9]')
+    btn_footer_decor = WebElement (xpath = '(//*[@data-elementor-type="footer"]/*[2]//span)[10]')
+    btn_footer_tools = WebElement (xpath = '(//*[@data-elementor-type="footer"]/*[2]//span)[11]')
+    hed_search = WebElement(xpath = '//*[@placeholder="Введите домены"]')
+    btn_search = WebElement(xpath ="(//*[contains(concat(' ', normalize-space(@class), ' '), ' m-button ')])[6]")
+    txt1 = WebElement (xpath = '((//*[@aria-hidden="false"])[1]//span)[8]')
+    txt2 = WebElement (xpath = '(//section//div)[11]')
+    var_dom = WebElement (xpath = '(//*[@name="choosedzones"])[1]/*[1]')
+    rez = WebElement (xpath = '(//*[@aria-hidden="false"])[1]')
 
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,12 +1,38 @@
+import time
+
 import pytest_check as check
 from Portfolio_S_Borozna.locators.locators_main_page import MainPage
 
 
-def test_headers(web_browser):
-    """Этот тест проверяет хедер главной страницы"""
+def test_search(web_browser):
+    """Этот тест проверяет поиск"""
 
     page = MainPage(web_browser)
 
+    elements_search = [ page.hed_search, page.btn_search]
 
-    check.equal(page.btn_headers_domain.get_text(), 'Домены', 'Тест локатора не равен ожидаймому результату')
-    check.is_true(page.btn_headers_mail.is_visible())
+
+    for el in elements_search:
+         check.is_true(el.is_visible())
+         check.is_true(el.is_clickable())
+
+    check.equal(page.hed_search.get_attribute('placeholder'),"Введите домены")
+
+    text_inp ='m/+\*)(^%!#$^&'
+    page.hed_search.send_keys(text_inp)
+    page.btn_search.click()
+    check.equal(page.txt1.get_text(), 'Имя домена должно содержать от 2 до 63 символов')
+    check.equal(page.txt2.get_text(), 'Символы "/", "+", "\", "*", ")", "(", "^", "%", "!", "#", "$", "&" удалены, '
+                           'т.к. являются недопустимыми.')
+    page.go_back()
+
+
+    inp = ['tryam','vivat','my']
+    for i in inp:
+        dom = i + page.var_dom.get_text()
+
+        page.hed_search.send_keys(i)
+        page.btn_search.click()
+        time.sleep(5)
+        check.not_equal(page.rez.get_text().find(dom),-1)
+        page.go_back()
